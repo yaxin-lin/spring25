84886fedf9d5c9359c5a48bed05a89da8161c049
Amber Ramirez <amber.ramirez216@gmail.com>
2025-02-20 20:24:08 -0800

    clicker cookie (work in progress)

---
+++ week3/amber_ramirez/script.js
@@ -0,0 +1,10 @@
+function addOneToCounter(){
+    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) + 1
+}
+function rain(){
+    let click= document.getElementById("counter")
+    if(click%50 === 0){
+        
+    }
+
+}
