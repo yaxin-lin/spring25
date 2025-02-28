e3a0ad9705918a0c7c502a04defd004a7428e3b8
s-farah <125221063+s-farah@users.noreply.github.com>
2025-02-27 19:21:52 -0800

    .

--- week 3/farah_shousha/script.js
+++
@@ -1,9 +0,0 @@
-function addOneToCounter(){
-    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) +1
-}
-
-let counter = 0;
-let level =1;
-let clicksInAMinute = 0;
-let clicksNedded = 10; 
-let timer = false; 
